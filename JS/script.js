e018dd5fb5f7fb1db26750bda8555efcf2abf96c
Shubham Dhage <dhageshubham96@gmail.com>
2021-10-10 19:43:46 +0530

    contact details

--- JS/script.js
+++ JS/script.js
@@ -8,4 +8,17 @@ $(document).ready(function () {
     $("#menu").removeClass("fa-times");
     $("header").removeClass("toggle");
   });
+
+  // smooth scrolling
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
 });
